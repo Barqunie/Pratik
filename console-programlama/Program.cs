3ceb7aa007e1858c4601a4218ac0426064041f25
Barqunie <118805901+Barqunie@users.noreply.github.com>
2022-12-01 09:51:28 +0300

    first commit

---
+++ console-programlama/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("İsminizi girin");
+string isim = Console.ReadLine();
+
+Console.WriteLine("Soyadınızı girin");
+string soyisim = Console.ReadLine();
+
+Console.WriteLine("Merhaba " +isim + " " +soyisim );
---
+++ degisken/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+DateTime dt = DateTime.Now;
+Console.WriteLine(dt);
+
+string str1 = string.Empty;
+str1 = "Barış Ayık";
+string ad = "Barış";
+string soyad ="Ayık";
+string tamİsim = ad + " " +soyad;
+
+Console.WriteLine(tamİsim);
+
+int integer1 = 4;
+int integer2 = 3;
+int integer3 = integer1 * integer2;
+
+Console.WriteLine(integer3);
+
+//değişken donusumu
+
+string str20 = "20";
+int int20 = 20;
+string yeniDeger = str20 + int20.ToString();
+
+Console.WriteLine(yeniDeger);
+
+int int21 = int20 + Convert.ToInt32(str20);
+
+Console.WriteLine(int21);
+
+ string aaa = "welcome to 30 days of code";
+
+ Console.WriteLine(aaa);
+
+ Console.WriteLine("Hello World");
